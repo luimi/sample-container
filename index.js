5094544e0d8f518fdedceaf51f2b08ef92ed2f77
Luis Miguel Meza Mendoza <luimi@live.com>
2023-05-09 18:40:09 -0500

    express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("started");
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
